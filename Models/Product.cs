353345b88de1f0d3db75023bb112b5e2e2d5b2c0
Fawadkhan0786 <130245230+Fawadkhan0786@users.noreply.github.com>
2023-04-13 23:53:56 +0530

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,23 @@ namespace MasterCRUDOperation.Models
     public class Product
     {
         [Key]
-        public int ProductId { get; set; }     
-        public string ProductName { get; set; }    
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; } 
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
     }
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+
+        // Foreign key 
+        [Display(Name = "Product")]
+        public virtual int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Category Products { get; set; }   
+    }
+
 }
